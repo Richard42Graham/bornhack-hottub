dbad0582d7ceead8cda34d58117b0cd08324cf4e
Jens Larsen <mr.javawolf@gmail.com>
2022-08-01 23:37:36 +0200

    Added: WIfi and HTTP

--- DS18x20_Temperature-Sensors-B/DS18x20_Temperature-Sensors-B.ino
+++ DS18x20_Temperature-Sensors-B/DS18x20_Temperature-Sensors-B.ino
@@ -1,4 +1,9 @@
 #include <OneWire.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 
 // OneWire DS18S20, DS18B20, DS1822 Temperature Example
 //
@@ -31,11 +36,100 @@ byte addr1[8] = {0x28, 0xA4, 0x25, 0x8C, 0x31, 0x21, 0x03, 0xED}; // sensor 1
 byte addr2[8] = {0x28, 0xAA, 0xB0, 0xFF, 0x52, 0x14, 0x01, 0x94}; // sensor 2
 byte addr3[8] = {0x28, 0xAA, 0x1F, 0xDD, 0x52, 0x14, 0x01, 0xBF}; // sensor 3
 
+float latest_temperature_1 = 0;
+float latest_temperature_2 = 0;
+float latest_temperature_3 = 0;
+
 unsigned long pool_frequency_ms = 5000;
 int temperature_acquisition_time_ms = 1000;
 unsigned long last_poll_time = 0;
 unsigned long temperature_start_read_time = 0;
 bool have_read_temperature = false;
+
+WiFiClient espClient;
+ESP8266WebServer httpServer(80);
+
+
+void handleRoot() {
+  httpServer.send(200, "text/html","Hey you found me dashboard @ https://richard42graham.github.io/bornhack-hottub or /data");
+}
+
+void handle_temperature_1() {
+  String output ="[";
+  output.concat(latest_temperature_1);
+  //for (int i = 0; i < 1000; i++) {
+  //  output.concat(records[i]);
+  //  if(i != 999){
+  //    output.concat(",");
+  //  }
+  //}
+  output.concat("]");
+  httpServer.sendHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  httpServer.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  httpServer.sendHeader("Access-Control-Allow-Origin", "*");
+  httpServer.send(200, "application/json",output);
+}
+
+void handle_temperature_2() {
+  String output ="[";
+  output.concat(latest_temperature_2);
+  //for (int i = 0; i < 1000; i++) {
+  //  output.concat(records[i]);
+  //  if(i != 999){
+  //    output.concat(",");
+  //  }
+  //}
+  output.concat("]");
+  httpServer.sendHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  httpServer.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  httpServer.sendHeader("Access-Control-Allow-Origin", "*");
+  httpServer.send(200, "application/json", output);
+}
+
+void handle_temperature_3() {
+  String output ="[";
+  output.concat(latest_temperature_2);
+  //for (int i = 0; i < 1000; i++) {
+  //  output.concat(records[i]);
+  //  if(i != 999){
+  //    output.concat(",");
+  //  }
+  //}
+  output.concat("]");
+  httpServer.sendHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  httpServer.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  httpServer.sendHeader("Access-Control-Allow-Origin", "*");
+  httpServer.send(200, "application/json", output);
+}
+
+void handle_battery() {
+  String output ="{";
+  output.concat("\"level\":");
+  output.concat(battery_level);
+  output.concat(",\"voltage\":");
+  output.concat(battery_voltage);
+  output.concat("}");
+  httpServer.sendHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  httpServer.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  httpServer.sendHeader("Access-Control-Allow-Origin", "*");
+  httpServer.send(200, "application/json", output);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += httpServer.uri();
+  message += "\nMethod: ";
+  message += (httpServer.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += httpServer.args();
+  message += "\n";
+  for (uint8_t i = 0; i < httpServer.args(); i++) {
+    message += " " + httpServer.argName(i) + ": " + httpServer.arg(i) + "\n";
+  }
+  httpServer.send(404, "text/plain", message);
+}
+
 
 void ReadBattery()
 {
@@ -95,14 +189,10 @@ float ReadTemperature(byte address[])
       raw = raw & ~3; // 10 bit res, 187.5 ms
     else if (cfg == 0x40)
       raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
+  /// default is 12 bit resolution, 750 ms conversion time
   }
   float celsius = (float)raw / 16.0;
   // fahrenheit = celsius * 1.8 + 32.0;
-  // Serial.print("Temperature = ");
-  // Serial.print(celsius);
-  // Serial.print(" Celsius, ");
-  // Serial.println(" ");
   return celsius;
 }
 
@@ -111,6 +201,38 @@ void setup(void)
   Serial.begin(115200);
   pinMode(A0, INPUT);
   ReadBattery();
+
+  // Wifi
+  delay(300);
+  // Setup wifi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("SSID", "Password");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  if (MDNS.begin("the-tub")) {
+    Serial.println("MDNS responder started");
+  }
+
+  // HTTP Server
+  httpServer.on("/data", HTTP_OPTIONS, []() {
+    httpServer.sendHeader("Access-Control-Max-Age", "10000");
+    httpServer.sendHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+    httpServer.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    httpServer.sendHeader("Access-Control-Allow-Origin", "*");
+    httpServer.send(200, "text/plain", "" );
+  });
+  httpServer.on("/", handleRoot);
+  httpServer.on("/data/temperature_1", handle_temperature_1);
+  httpServer.on("/data/temperature_2", handle_temperature_2);
+  httpServer.on("/data/temperature_3", handle_temperature_3);
+  httpServer.on("/data/battery", handle_battery);
+  httpServer.onNotFound(handleNotFound);
+  httpServer.begin();
 }
 
 void loop(void)
@@ -143,6 +265,9 @@ void loop(void)
     Serial.print(", temperature3: ");
     Serial.print(temperature3);
     Serial.println(" ");
+    latest_temperature_1 = temperature1;
+    latest_temperature_2 = temperature2;
+    latest_temperature_3 = temperature3;
     have_read_temperature = true;
   }
 }
